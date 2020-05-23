2b84c9e341f753be7d9ff6d72b53c524d32f8cd5
itkhan <itkhanz@gmail.com>
2020-05-23 05:12:13 +0200

    Detecting Multiple Faces

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import './FaceRecognition.css';
+import React from 'react';
+import BorderBox from './BorderBox';
 
-const FaceRecognition = ({imageUrl, box}) => {
+const FaceRecognition = ({imageUrl, box, boxAll}) => {
+    // console.log(boxAll);
     return (
         <div className='center ma'>
             <div className="absolute mt2">
                 <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />
-                <div 
+                {/* <div 
                     className = 'bounding-box' 
                     style = {{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}
-                ></div>
+                ></div> */}
+                {
+                    boxAll.map((box, i) => {
+                        return (
+                            <BorderBox
+                                key = {boxAll[i].topRow}
+                                top={boxAll[i].topRow}
+                                right={boxAll[i].rightCol}
+                                bottom={boxAll[i].bottomRow}
+                                left={boxAll[i].leftCol}
+                            />
+                        );
+                    })
+                }
             </div>
         </div>
     )
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = () => {
     return (
         <div>
             <div className="white f3">
-                {'Andrei, your current rank is...'}
+                {'Ibtisam, your current rank is...'}
             </div>
             <div className="white f1">
                 {'#5'}
